a34f8b11f9f126dc401056530b88455fe9cdaf1c
Patrick McCorry <stonecoldpat@gmail.com>
2020-04-19 18:18:53 +0100

    first commit

---
+++ src/ts/config.ts
@@ -0,0 +1,13 @@
+// any.sender API configuration
+export const MINIMUM_ANYSENDER_DEADLINE = 410; // It is 400, but this provides some wiggle room.
+export const ANYSENDER_RELAY_CONTRACT =
+  "0xa404d1219Ed6Fe3cF2496534de2Af3ca17114b06"; // On-chain relay contract
+export const ANYSENDER_API = "https://api.anydot.dev/any.sender.mainnet"; // API Link
+export const RECEIPT_KEY = "0x02111c619c5b7e2aa5c1f5e09815be264d925422"; // Any.sender operator signing key
+export const DEPOSIT_CONFIRMATIONS = 40; // Must wait this long before any.sender recognises deposit
+
+// Oracle and Infura configuration
+export const INFURA_PROJECT_ID = "";
+export const NETWORK_NAME = "mainnet";
+export const ORACLE_CONTRACT_ADDRESS = "";
+export const ORACLE_MNEMONIC = "";
---
+++ src/ts/submitBeacon.ts
@@ -0,0 +1,152 @@
+//      ___      .__   __. ____    ____      _______. _______ .__   __.  _______   _______ .______
+//     /   \     |  \ |  | \   \  /   /     /       ||   ____||  \ |  | |       \ |   ____||   _  \
+//    /  ^  \    |   \|  |  \   \/   /     |   (----`|  |__   |   \|  | |  .--.  ||  |__   |  |_)  |
+//   /  /_\  \   |  . `  |   \_    _/       \   \    |   __|  |  . `  | |  |  |  ||   __|  |      /
+//  /  _____  \  |  |\   |     |  |  __ .----)   |   |  |____ |  |\   | |  '--'  ||  |____ |  |\  \----.
+// /__/     \__\ |__| \__|     |__| (__)|_______/    |_______||__| \__| |_______/ |_______|| _| `._____|
+//                          ______   ______   .___  ___. .______    __
+//                         /      | /  __  \  |   \/   | |   _  \  |  |
+//                        |  ,----'|  |  |  | |  \  /  | |  |_)  | |  |
+//                        |  |     |  |  |  | |  |\/|  | |   ___/  |  |
+//                        |  `----.|  `--'  | |  |  |  | |  |      |__|
+//                         \______| \______/  |__|  |__| | _|      (__)
+
+import { ethers, Wallet } from "ethers";
+import { Provider } from "ethers/providers";
+
+import {
+  sendToAnySender,
+  watchRelayTx,
+  onchainDepositFor,
+  getAnySenderBalance,
+  consolelog,
+} from "./utils";
+import {
+  INFURA_PROJECT_ID,
+  ORACLE_MNEMONIC,
+  NETWORK_NAME,
+  DEPOSIT_CONFIRMATIONS,
+  ORACLE_CONTRACT_ADDRESS,
+} from "./config";
+import { parseEther } from "ethers/utils";
+import { CommunityOracleFactory } from "../typedContracts/CommunityOracleFactory";
+
+/**
+ * Set up the provider and wallet
+ */
+async function setup() {
+  const infuraProvider = new ethers.providers.InfuraProvider(
+    NETWORK_NAME,
+    INFURA_PROJECT_ID
+  );
+
+  const userMnemonicWallet = ethers.Wallet.fromMnemonic(ORACLE_MNEMONIC);
+  const user = userMnemonicWallet.connect(infuraProvider);
+
+  return {
+    user,
+    provider: infuraProvider,
+  };
+}
+
+/**
+ * Submit beacon to oracle contract
+ * @param message Message to submit
+ * @param user User's wallet
+ * @param provider InfuraProvider
+ * @param oracleCon Oracle Contract
+ */
+async function sendBeacon(
+  beacon: string,
+  user: Wallet,
+  provider: Provider,
+  oracleCon: ethers.Contract
+) {
+  // Encode function and call data
+  const callData = oracleCon.interface.functions.submitBeacon.encode([beacon]);
+
+  // Submits beacon to any.sender
+  const { relayTx } = await sendToAnySender(
+    oracleCon,
+    callData,
+    user,
+    provider
+  );
+
+  // Wait for transaction to get mined
+  const totalWait = await watchRelayTx(relayTx, user, provider);
+  consolelog("Relay transaction confirmed after " + totalWait + " blocks");
+  const processed = await oracleCon.oracleSubmitted(user.address);
+  consolelog(
+    "Oracle contract has received your beacon submission: " + processed
+  );
+}
+
+/**
+ * We have filled in most of the program for you.
+ * Go to sendTicket() to fill in the blanks.
+ */
+(async () => {
+  // ricmoo this is ur fault
+  console.log = () => {};
+
+  // Sanity check the config.ts is filled in.
+  if (ORACLE_MNEMONIC.length === 0 || INFURA_PROJECT_ID.length === 0) {
+    consolelog(
+      "Please open config.ts and fill in ORACLE_MNEMONIC / INFURA_PROJECT_ID"
+    );
+    return;
+  }
+
+  // Set up wallets & provider
+  const { user, provider } = await setup();
+
+  // Get CyberDice contract
+  const oracleCon = new CommunityOracleFactory(user).attach(
+    ORACLE_CONTRACT_ADDRESS
+  );
+
+  consolelog(
+    "*** You are running the any.sender competition script on " +
+      NETWORK_NAME +
+      " ***"
+  );
+
+  consolelog("Your wallet address: " + user.address);
+
+  const registered = await oracleCon.appointedOracle(user.address);
+  consolelog("Registered as an appointed oracle: " + registered);
+
+  const submitted = await oracleCon.oracleSubmitted(user.address);
+  consolelog("Have you already submitted? " + submitted);
+
+  // Sanity check minimum balance.
+  const bal = await provider.getBalance(user.address);
+  if (bal.lt(parseEther("0.03"))) {
+    consolelog(
+      "Your balance is " +
+        bal.toString() +
+        ". Please top it up to 0.03 eth or more."
+    );
+    return;
+  }
+
+  // We need to wait for on-chain confirmations before any.sender
+  // accepts the deposit.
+  consolelog(
+    "Sending on-chain deposit - wait ~" +
+      DEPOSIT_CONFIRMATIONS +
+      " confirmations"
+  );
+  await onchainDepositFor(parseEther("0.029"), user);
+
+  // What is the user's any.sender balance?
+  const balance = await getAnySenderBalance(user);
+  consolelog("Balance on any.sender: " + balance.toString() + " wei");
+
+  // Send ticket to any.sender
+  await sendBeacon("123", user, provider, oracleCon);
+})().catch((e) => {
+  consolelog(e);
+  // Deal with the fact the chain failed
+});
